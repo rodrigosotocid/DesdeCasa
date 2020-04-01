5577fc96d271321205c1ccf9d3074ef76e0bc43a
istikis <istikis@gmail.com>
2020-04-01 12:43:32 +0200

    ..

---
+++ appclient/js/main.js
@@ -0,0 +1,7 @@
+//Fichero javascript para app
+
+console.info("Esto lo puedes ver depurado en el navegador");
+console.debug("esto es una traza de tipo debug");
+console.trace("esto se usa para tracear o decir que entras y sales");
+console.warn("Mensaje de Warning!");
+console.error("Ha petado nuestra App");
