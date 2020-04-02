fd4a1d57a9c017f5eb5d529a1b9367261bacbcba
istikis <istikis@gmail.com>
2020-04-02 09:53:51 +0200

    añadiendo un poco de JS. Ejercicio lista y ejem...

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -5,3 +5,11 @@ console.debug("esto es una traza de tipo debug");
 console.trace("esto se usa para tracear o decir que entras y sales");
 console.warn("Mensaje de Warning!");
 console.error("Ha petado nuestra App");
+
+
+let lista = document.getElementById('lista');
+lista.innerHTML = '';
+
+for(let i=1; i < 7; i++){
+    lista.innerHTML += `<li><img src="img/avatar${i}.png" alt="avatar"></li>`;
+}
