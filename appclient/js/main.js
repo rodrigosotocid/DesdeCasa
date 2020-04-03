b8b4c280868d87ddbf0780b43911e2cf1cd4d94c
istikis <istikis@gmail.com>
2020-04-03 11:03:24 +0200

    creación personas.html

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -6,11 +6,49 @@ console.trace("esto se usa para tracear o decir que entras y sales");
 console.warn("Mensaje de Warning!");
 console.error("Ha petado nuestra App");
 
+window.addEventListener('load', init());
 
-let lista = document.getElementById('lista');
-lista.innerHTML = '';
+function init(){
+    console.debug('Document Load and Ready');
+    // es importante esperar que todo este cragando para comenzar
 
-for(let i=1; i < 7; i++){
-    lista.innerHTML += `<li><img src="img/avatar${i}.png" alt="avatar"></li>`;
-}
+    const personas = [
+        {
+            "nombre" : "Oconnor",
+            "avatar" : "img/avatar1.png",
+            "sexo"   : "h"
+        },
+        {
+            "nombre" : "Pepa",
+            "avatar" : "img/avatar2.png",
+            "sexo"   : "m"
+        },
+        {
+            "nombre" : "JoseMAri",
+            "avatar" : "img/avatar3.png",
+            "sexo"   : "h"
+        }
+    ];
+
+
+    //seleccionar la lista por id
+    let lista = document.getElementById('alumnos');
+    lista.innerHTML = ''; // vaciar html 
+
+    for(let i=0; i < personas.length; i++){
+        const persona = personas[i];
+        lista.innerHTML += `<li><img src="${persona.avatar}" alt="avatar">${persona.nombre}</li>`;
+    }
+
+    /*
+    * let lista = document.getElementById('lista');
+    * lista.innerHTML = '';
+    *
+    * for(let i=1; i < 7; i++){
+    *   lista.innerHTML += `<li><img src="img/avatar${i}.png" alt="avatar"></li>`;
+    * }
+    */
+}//init
+
+
 
