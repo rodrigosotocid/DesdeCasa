684c01034aca878d14135ddac0ee54949e6178b3
istikis <istikis@gmail.com>
2020-04-06 18:04:13 +0200

    programación funcional

---
+++ appclient/js/test.js
@@ -0,0 +1,18 @@
+// "duration" es en minutos
+ 
+var tasks = [
+ 
+    {
+      'name'     : 'Write for Envato Tuts+',
+      'duration' : 120
+    },
+    {
+      'name'     : 'Work out',
+      'duration' : 60
+    },
+    {
+      'name'     : 'Procrastinate on Duolingo',
+      'duration' : 240
+    }
+   
+  ];
