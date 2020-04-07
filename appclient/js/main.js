c25249c7752ad254aa99005f184e0d25e18b1dce
istikis <istikis@gmail.com>
2020-04-07 11:11:32 +0200

    ...

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -1,54 +1,55 @@
 //Fichero javascript para app
 
+/* 
 console.info("Esto lo puedes ver depurado en el navegador");
 console.debug("esto es una traza de tipo debug");
 console.trace("esto se usa para tracear o decir que entras y sales");
 console.warn("Mensaje de Warning!");
-console.error("Ha petado nuestra App");
+console.error("Ha petado nuestra App"); 
+*/
 
-window.addEventListener('load', init());
+const personas = [
+  {
+    nombre: "Oconnor",
+    avatar: "img/avatar1.png",
+    sexo: "h",
+  },
+  {
+    nombre: "Pepa",
+    avatar: "img/avatar2.png",
+    sexo: "m",
+  },
+  {
+    nombre: "JoseMAri",
+    avatar: "img/avatar3.png",
+    sexo: "h",
+  },
+];
 
-function init(){
-    console.debug('Document Load and Ready');
-    // es importante esperar que todo este cragando para comenzar
+window.addEventListener("load", init());
 
-    const personas = [
-        {
-            "nombre" : "Oconnor",
-            "avatar" : "img/avatar1.png",
-            "sexo"   : "h"
-        },
-        {
-            "nombre" : "Pepa",
-            "avatar" : "img/avatar2.png",
-            "sexo"   : "m"
-        },
-        {
-            "nombre" : "JoseMAri",
-            "avatar" : "img/avatar3.png",
-            "sexo"   : "h"
-        }
-    ];
+function init() {
+  // es importante esperar que todo este cargando para comenzar
+  console.debug("Document Load and Ready");
 
+  /* pintarLista(personasFiltradas);
+  console.log(personasFiltradas); */
+} //init
 
-    //seleccionar la lista por id
-    let lista = document.getElementById('alumnos');
-    lista.innerHTML = ''; // vaciar html 
+function pintarLista(arrayPersonas) {
+  //seleccionar la lista por id
+  let lista = document.getElementById("alumnos");
+  lista.innerHTML = "";
+  arrayPersonas.forEach(
+    (p) =>
+      (lista.innerHTML += `<li><img src="${p.avatar}" alt="avatar">${p.nombre}</li>`)
+  );
+}
 
-    for(let i=0; i < personas.length; i++){
-        const persona = personas[i];
-        lista.innerHTML += `<li><img src="${persona.avatar}" alt="avatar">${persona.nombre}</li>`;
-    }
-
-    /*
-    * let lista = document.getElementById('lista');
-    * lista.innerHTML = '';
-    *
-    * for(let i=1; i < 7; i++){
-    *   lista.innerHTML += `<li><img src="img/avatar${i}.png" alt="avatar"></li>`;
-    * }
-    */
-}//init
-
-
-
+function seleccionSexo() {
+  let option = document.getElementById("selectorSexo").value;
+  console.log(option);
+  const personasFiltradas = personas.filter((p) => p.sexo == `${option}`);
+  console.log(personasFiltradas);
+  pintarLista(personasFiltradas);
+}
