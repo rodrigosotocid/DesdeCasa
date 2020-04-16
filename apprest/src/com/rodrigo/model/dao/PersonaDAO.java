ec6c2ac05e0290d4d930b7d26fbf7f02be9633c2
istikis <istikis@gmail.com>
2020-04-16 10:42:09 +0200

    implentación mapper(rs)

--- apprest/src/com/rodrigo/model/dao/PersonaDAO.java
+++ apprest/src/com/rodrigo/model/dao/PersonaDAO.java
@@ -6,49 +6,51 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.rodrigo.model.Persona;
 
 public class PersonaDAO implements IDAO<Persona> {
 
-	//SINGLETON
+	private static final Logger LOGGER = Logger.getLogger(PersonaDAO.class.getCanonicalName());
+
 	private static PersonaDAO INSTANCE = null;
-	
+
+	private static String SQL_GET_ALL = "SELECT id, nombre, avatar,sexo FROM persona ORDER BY id DESC LIMIT 500;";
+	private static String SQL_GET_BY_ID = "SELECT id, nombre, avatar, sexo FROM persona WHERE id = ?;";
+	private static String SQL_DELETE = "DELETE FROM persona WHERE id = ?;";
+	private static String SQL_INSERT = "INSERT INTO persona (nombre, avatar, sexo) VALUES(?,?,?);";
+	private static String SQL_UPDATE = "UPDATE persona SET nombre = ?, avatar = ?, sexo = ? WHERE id = ?;";
+
 	// constructor privado
 	private PersonaDAO() {
 		super();
 	}
 
 	public synchronized static PersonaDAO getInstance() {
-		if(INSTANCE ==null) {
+		if (INSTANCE == null) {
 			INSTANCE = new PersonaDAO();
 		}
 		return INSTANCE;
 	}
-	// FIN SINGLETON
-	
-	
+
 	@Override
 	public List<Persona> getAll() {
 
+		LOGGER.info("getAll");
+
 		ArrayList<Persona> registros = new ArrayList<Persona>();
-		String sql = "SELECT id, nombre, avatar, sexo FROM persona ORDER BY id DESC LIMIT 500";
 
 		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(sql);
+				PreparedStatement pst = con.prepareStatement(SQL_GET_ALL);
 				ResultSet rs = pst.executeQuery();
 
 		) {
+			LOGGER.info(pst.toString());
 
-			while( rs.next() ) {
-				
-				Persona p = new Persona();
-				p.setId( rs.getInt("id") );
-				p.setNombre( rs.getString("nombre"));
-				p.setAvatar(rs.getString("avatar"));
-				p.setSexo(rs.getString("sexo"));
-				
-				registros.add(p);
+			while (rs.next()) {
+				//devuelve una persona y lo añade a la lista
+				registros.add(mapper(rs));
 			}
 		} catch (SQLException e) {
 
@@ -58,10 +60,36 @@ public class PersonaDAO implements IDAO<Persona> {
 		return registros;
 	}
 
-
 	@Override
 	public Persona getById(int id) throws Exception {
-		throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
+		
+		LOGGER.info("getById");
+
+		Persona registro = null;
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_GET_BY_ID);
+
+		) {
+			pst.setInt(1, id);
+			LOGGER.info(pst.toString());
+
+			try(ResultSet rs = pst.executeQuery()){
+				
+				if (rs.next()) {
+					
+					registro = mapper(rs);
+					
+				}else {
+					throw new Exception("Registro no encontrado para Id " + id);
+				}
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return registro;
 	}
 
 	@Override
@@ -77,6 +105,15 @@ public class PersonaDAO implements IDAO<Persona> {
 	@Override
 	public Persona update(Persona pojo) throws Exception, SQLException {
 		throw new UnsupportedOperationException("NO ESTA IMPLEMENTADO");
+	}
+	
+	private Persona mapper(ResultSet rs) throws SQLException {
+		Persona p = new Persona();
+		p.setId(rs.getInt("id"));
+		p.setNombre(rs.getString("nombre"));
+		p.setAvatar(rs.getString("avatar"));
+		p.setSexo(rs.getString("sexo"));
+		return p;
 	}
 
 }
