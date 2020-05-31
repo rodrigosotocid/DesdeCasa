3990b303657f59d5c577538a7f333b767f22372a
istikis <istikis@gmail.com>
2020-05-31 19:30:00 +0200

    BBDD actualizada a V3.0

--- apprest/alumnos_database.sql
+++ apprest/alumnos_database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `alumnos` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `alumnos`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: alumnos
@@ -27,9 +29,12 @@ CREATE TABLE `curso` (
   `nombre` varchar(150) COLLATE utf8_spanish_ci NOT NULL,
   `imagen` varchar(150) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'default.png',
   `precio` decimal(10,0) NOT NULL,
+  `persona_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`),
+  KEY `persona_id_idx` (`persona_id`),
+  CONSTRAINT `persona_id` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +43,7 @@ CREATE TABLE `curso` (
 
 LOCK TABLES `curso` WRITE;
 /*!40000 ALTER TABLE `curso` DISABLE KEYS */;
-INSERT INTO `curso` VALUES (1,'Python','pyn.png',255),(2,'Networking ','redes.png',235),(3,'Android','android.png',375),(4,'Java 8','java.png',395),(5,'Angular JS','angular.png',300),(6,'React JS','react.png',300);
+INSERT INTO `curso` VALUES (2,'Networking ','redes.png',235,1),(3,'Android','android.png',375,97),(4,'Java 8','java.png',395,108),(5,'Angular JS','angular.png',300,1),(6,'React JS','react.png',300,1),(7,'Python','pyn.png',255,1);
 /*!40000 ALTER TABLE `curso` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,9 +87,12 @@ CREATE TABLE `persona` (
   `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `avatar` varchar(250) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'avatar1.png',
   `sexo` varchar(1) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'h',
+  `rol_id` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=97 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`),
+  KEY `fk_persona_rol1_idx` (`rol_id`),
+  CONSTRAINT `rol_id` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +101,7 @@ CREATE TABLE `persona` (
 
 LOCK TABLES `persona` WRITE;
 /*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-INSERT INTO `persona` VALUES (1,'Arantxa','avatar1.png','m'),(37,'Juan','avatar13.png','h'),(52,'Álvaro','avatar9.png','h'),(54,'Charo','avatar10.png','m'),(55,'Joseba','avatar6.png','h'),(57,'Rodrigo','avatar14.png','h'),(61,'Eneritz','avatar16.png','m'),(62,'Janire','avatar15.png','m'),(63,'Lenny','avatar6.png','h'),(65,'Mariela','avatar7.png','m'),(88,'Ander','avatar3.png','h');
+INSERT INTO `persona` VALUES (1,'Mari','avatar4.png','m',2),(37,'Juan','avatar13.png','h',1),(52,'Álvaro','avatar9.png','h',1),(54,'Charo','avatar10.png','m',1),(55,'Joseba','avatar6.png','h',1),(57,'Rodrigo','avatar14.png','h',2),(61,'Eneritz','avatar16.png','m',1),(62,'Janire','avatar15.png','m',1),(63,'Jaime','avatar11.png','h',2),(65,'Manuela','avatar7.png','m',1),(88,'Ander','avatar3.png','h',2),(97,'Zuriñe','avatar1.png','m',2),(98,'Loquita','avatar1.png','m',1),(101,'Romualdo','avatar11.png','h',1),(108,'Estela','avatar10.png','m',2);
 /*!40000 ALTER TABLE `persona` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,8 +116,9 @@ CREATE TABLE `persona_has_curso` (
   `persona_id` int NOT NULL,
   `curso_id` int NOT NULL,
   PRIMARY KEY (`persona_id`,`curso_id`),
-  KEY `fk_persona_has_curso_curso1_idx` (`curso_id`),
-  KEY `fk_persona_has_curso_persona_idx` (`persona_id`),
+  KEY `persona_id_idx` (`persona_id`),
+  KEY `fk_persona_has_curso_curso_idx` (`curso_id`),
+  CONSTRAINT `fk_persona_has_curso_curso` FOREIGN KEY (`curso_id`) REFERENCES `curso` (`id`),
   CONSTRAINT `fk_persona_has_curso_persona` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -120,9 +129,278 @@ CREATE TABLE `persona_has_curso` (
 
 LOCK TABLES `persona_has_curso` WRITE;
 /*!40000 ALTER TABLE `persona_has_curso` DISABLE KEYS */;
-INSERT INTO `persona_has_curso` VALUES (1,1),(37,1),(54,1),(57,1),(54,2),(57,2),(37,3),(55,3),(57,3),(37,4),(57,4),(88,4),(1,5),(65,5),(1,6),(65,6);
+INSERT INTO `persona_has_curso` VALUES (1,2),(1,6),(37,2),(37,3),(37,4),(37,5),(37,6),(52,3),(52,5),(55,3),(65,5),(65,6),(88,4),(97,2),(97,6),(98,3),(98,6),(101,3),(101,4),(101,5);
 /*!40000 ALTER TABLE `persona_has_curso` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rol` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(100) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'alumno',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `tipo_UNIQUE` (`tipo`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol`
+--
+
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES (1,'alumno'),(2,'profesor');
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'alumnos'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `alumnosGetAll` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `alumnosGetAll`()
+BEGIN
+SELECT 
+p.id as persona_id, 
+p.nombre as persona_nombre, 
+p.avatar as persona_avatar, 
+p.sexo as persona_sexo, 
+p.rol_id as persona_rol,
+c.id as curso_id, 
+c.nombre as curso_nombre,
+c.precio as curso_precio, 
+c.imagen as curso_imagen, 
+c.persona_id as curso_profesor, 
+r.id as rol_id,
+r.tipo as rol_tipo,
+pf.nombre as profesor_nombre 
+FROM (persona p LEFT JOIN persona_has_curso pc ON p.id = pc.persona_id) 
+LEFT JOIN curso c ON pc.curso_id =  c.id 
+LEFT JOIN rol r ON p.rol_id = r.id 
+LEFT JOIN persona pf ON c.persona_id = pf.id 
+WHERE r.tipo LIKE 'alumno';
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `cursosGetAll` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cursosGetAll`()
+BEGIN
+SELECT 
+c.id as curso_id, 
+c.nombre as curso_nombre,
+c.imagen as curso_imagen,
+c.precio as curso_precio, 
+p.id as profesor_id,
+p.nombre as profesor_nombre, 
+p.avatar as profesor_avatar, 
+p.sexo as profesor_sexo, 
+p.rol_id as profesor_rol 
+FROM curso c 
+LEFT JOIN persona p ON c.persona_id = p.id 
+ORDER BY c.id desc
+LIMIT 500;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `cursosGetById` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cursosGetById`(IN _id INT)
+BEGIN
+SELECT 
+c.id as curso_id, 
+c.nombre as curso_nombre,
+c.imagen as curso_imagen,
+c.precio as curso_precio, 
+p.id as profesor_id,
+p.nombre as profesor_nombre, 
+p.avatar as profesor_avatar, 
+p.sexo as profesor_sexo, 
+p.rol_id as profesor_rol 
+FROM curso c 
+LEFT JOIN persona p ON c.persona_id = p.id 
+WHERE c.id = _id ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `personaDelete` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `personaDelete`(IN _id INT)
+BEGIN
+DELETE FROM persona WHERE id = _id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `personaInsert` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `personaInsert`(IN _nombre VARCHAR(50), IN _avatar VARCHAR(250), IN _sexo VARCHAR(1), _rol_id INT,  OUT _id INT)
+BEGIN
+INSERT INTO persona ( nombre, avatar, sexo, rol_id) VALUES ( _nombre, _avatar, _sexo, _rol_id);
+SET _id = LAST_INSERT_ID();
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `personasGetAll` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `personasGetAll`()
+BEGIN
+SELECT
+p.id as persona_id,
+p.nombre as persona_nombre, 
+p.avatar as persona_avatar,
+p.sexo as persona_sexo,
+p.rol_id as persona_rol,
+c.id as curso_id,
+c.nombre as curso_nombre,
+c.precio as curso_precio,
+c.imagen  as curso_imagen,
+r.id as rol_id,
+r.tipo as rol_tipo
+FROM (persona p LEFT JOIN persona_has_curso pc ON p.id = pc.persona_id)
+LEFT JOIN curso c ON pc.curso_id = c.id
+LEFT JOIN rol r ON p.rol_id = r.id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `personasGetById` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `personasGetById`(IN _id INT)
+BEGIN
+SELECT
+p.id as persona_id,
+p.nombre as persona_nombre,
+p.avatar as persona_avatar,
+p.sexo as persona_sexo,
+p.rol_id as persona_rol,
+c.id as curso_id,
+c.nombre as curso_nombre,
+c.precio as curso_precio,
+c.imagen  as curso_imagen
+FROM (persona p LEFT JOIN persona_has_curso pc ON p.id = pc.persona_id)
+LEFT JOIN curso c ON pc.curso_id = c.id WHERE p.id = _id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `profesoresGetAll` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `profesoresGetAll`()
+BEGIN
+SELECT 
+p.id as profesor_id, 
+p.nombre as profesor_nombre, 
+p.avatar as profesor_avatar,
+p.sexo as profesor_sexo, 
+p.rol_id as profesor_rol,
+c.id as curso_id, 
+c.nombre as curso_nombre, 
+c.precio as curso_precio,
+c.imagen as curso_imagen, 
+c.persona_id as profesor_id,
+r.id as rol_id,
+r.tipo as rol_tipo
+FROM persona p 
+LEFT JOIN curso c ON c.persona_id = p.id 
+LEFT JOIN rol r ON p.rol_id = r.id 
+WHERE r.tipo LIKE 'profesor'
+ORDER BY p.id
+LIMIT 100;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,21 +411,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-05 16:35:28
-
-
-
-/*CUSTOM SQL*/
-/*
-SELECT
-	p.id as persona_id,
-    p.nombre as persona_nombre,
-    p.avatar as persona_avatar,
-    p.sexo as persona_sexo,
-    c.id as curso_id,
-    c.nombre as curso_nombre,
-    c.imagen as curso_imagen,
-    c.precio as curso_precio
-FROM (persona p LEFT JOIN persona_has_curso pc ON p.id = pc.persona_id)
-LEFT JOIN curso c ON pc.curso_id = c.id;
-*/
+-- Dump completed on 2020-05-31 19:27:17
